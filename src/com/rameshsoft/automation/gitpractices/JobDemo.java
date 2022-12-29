d75d49ef00d35bdae8e53cf23a8f689055222dd2
Mohammad Adnan <md.adnan135@gmail.com>
2022-12-28 21:57:58 -0500

    added new file

--- src/com/rameshsoft/automation/gitpractices/JobDemo.java
+++ src/com/rameshsoft/automation/gitpractices/JobDemo.java
@@ -7,6 +7,10 @@ public static void main(String[] args) {
 	System.out.println("job");
 	System.out.println("practice");
 	System.out.println("Twitter");
+	System.out.println("eclipse");
+	
+	
+	
 }
 }
 
